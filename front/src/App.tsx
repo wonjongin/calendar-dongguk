7bab231835f2eb2f34b00d5d520bbc867420f965
Won, J.I <wji0227@naver.com>
2025-09-14 20:37:31 +0900

    feat: Add markdown renderer for main page

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,24 @@
+import Markdown from "react-markdown";
 import "./App.css";
-import CalendarComponent from "./Calendar";
+import { useEffect, useState } from "react";
+import remarkGfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
 
 function App() {
+  const [readme, setReadme] = useState("");
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/wonjongin/calendar-dongguk/main/README.md",
+    )
+      .then((res) => res.text())
+      .then((text) => setReadme(text));
+  }, []);
+
   return (
-    <div>
-      <CalendarComponent />
+    <div className="container markdown mt-0 sm:mt-10 p-8 sm:p-10 bg-white dark:bg-gray-800 rounded-lg shadow-lg">
+      <Markdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
+        {readme}
+      </Markdown>
     </div>
   );
 }
